ab3f0f94411941ec0129d5b258ddd1091cda66fa
Xael <67170424+Xael@users.noreply.github.com>
2025-12-15 11:00:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const NFeService = require('./services/NFeService');
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
